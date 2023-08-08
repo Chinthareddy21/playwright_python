d96ab514f5a1390da97829c0cd937c0f52efbafd
Bhargava Reddy Chinthareddy <114492951+Chinthareddy21@users.noreply.github.com>
2023-08-08 14:51:54 +0530

    login fixture added

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import Playwright, expect
 import pytest
-
 from lib.url_s import URL_s
+from objectRepository.loginObjects import Login_Objects
+from lib.credentials import Credentials
+
 
 # Assertions time out
 expect.set_options(timeout=60_000)
@@ -9,8 +11,10 @@ expect.set_options(timeout=60_000)
 @pytest.fixture(scope="session")
 def Home_page(playwright: Playwright):
     # Browser and page set up
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    browser_Chrome = playwright.chromium.launch(headless=False)
+    browser_Firefox = playwright.firefox.launch(headless=False)
+    browser_Webkit = playwright.webkit.launch(headless=False)
+    context = browser_Chrome.new_context()
     page = context.new_page()
 
     url = URL_s
@@ -30,4 +34,47 @@ def Home_page(playwright: Playwright):
     
     # Browser close
     context.close()
-    browser.close()
+
+
+@pytest.fixture(scope="session")
+def login(playwright: Playwright):
+    # Browser and page set up
+    browser_Chrome = playwright.chromium.launch(headless=False)
+    browser_Firefox = playwright.firefox.launch(headless=False)
+    browser_Webkit = playwright.webkit.launch(headless=False)
+    context = browser_Chrome.new_context()
+    page = context.new_page()
+   
+    # Constructors
+    login = Login_Objects
+    url = URL_s
+    credentials = Credentials
+        
+    # Homepage navigation
+    page.goto(url.Base_url)
+
+    # Getting homepage API status code
+    page.request.get(url.Base_url)
+
+    # printing homepage API status code
+    print(page.request.get(url.Base_url))
+
+    # Step to login page navigation
+    page.locator(login.account_button).click()
+    # Login page screenshot
+    page.screenshot(path="Screenshots/login/login page.png")
+    # Entering valid username & password
+    page.locator(login.email_editbox_input).fill(credentials.username)
+    page.locator(login.password_editbox_input).fill(credentials.password)
+    # Clicking on login button
+    page.locator(login.login_button).click()
+
+    # User is navigated to homepage after successfully logged in
+    expect(page).to_have_url(url.Base_url)
+    # Verifying wether user successfully logged in or not 
+    expect(page.locator(login.account_button_after_login)).to_have_text("Hello, Skyreaper")
+
+    yield page
+    
+    # Browser close
+    context.close()
