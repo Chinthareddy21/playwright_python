b09cf899a82df06027ad0ddb1c81d6b403538a12
Bhargava Reddy Chinthareddy <114492951+Chinthareddy21@users.noreply.github.com>
2023-08-19 11:15:02 +0530

    Small changes

--- test/lib/credentials.py
+++ test/lib/credentials.py
@@ -12,28 +12,13 @@ class Credentials:
     # products
     Product_1 = "shirt"
     Product_2 = "watches"
-    Product_3 = "Police Avondale Stainless Steel Navy Dial Bracelet Watch Police Avondale Stainless Steel Navy Dial Bracelet Watch Police R 5295,00"
+    Product_3 = ""
     Product_4 = ""
     Product_5 = ""
 
     # Search page title
     Product_1_search_title = "shirt - Bash.com"
     Product_2_search_title = "watches - Bash.com"
-
-    # Check out details
-    Product_name = '\
-        Police Avondale Stainless Steel Navy Dial Bracelet Watch\
-        \
-        Police\
-        \
-        \
-        Qty: 1 \
-        \
-        \
-        '
-    
-    bash_product_UUID = 'b2bdfbf9-3f3b-441e-bc93-acd80c3bd1ba'
-    Product_code = '01A722AAEB8'
 
     # Email for user sign up
     Email = 'test220022@gmail.com'
--- test/test_Add_To_Cart.py
+++ test/test_Add_To_Cart.py
@@ -1,24 +1,12 @@
-from lib.credentials import Credentials
-from lib.url_s import URL_s
-from objectRepository.addToCartObjects import Add_To_Cart
-
 """
         To test login functionality
         Run command: pytest test/test_Add_To_Cart.py
 """
 
 
-def test_add_to_cart(login):
+def test_add_to_cart(login, logger):
     # Constructors
     page = login
-    url = URL_s
-    credentials = Credentials
-
-    # Getting homepage after login API status code
-    page.request.get(url.Base_url)
-
-    # printing homepage after login API status code
-    print(page.request.get(url.Base_url))
 
     # Selecting category
     page.get_by_role("link", name="Men", exact=True).first.click()
@@ -27,41 +15,60 @@ def test_add_to_cart(login):
     # Adding item to cart
     page.get_by_role("button", name="Add to cart").click()
 
+    if "1" in page.frame_locator('.zoid-visible').locator('[class="change-quantity__InputValue-bQwA-dl OfyKm"]').text_content():
+        logger.info("Item is added to cart")
+    else:
+        logger.info("Item failed to add to cart")
+
     # Add to cart screenshot
     page.screenshot(path="Screenshots/add_to_cart/added to cart.png")
 
 
-def test_adding(login):
+def test_adding(login, logger):
     # Constructors
     page = login
-    add_to_cart = Add_To_Cart
 
     # increasing quantity
     page.frame_locator('.zoid-visible').get_by_alt_text('plus').click()
+
+    if "2" in page.frame_locator('.zoid-visible').locator('[class="change-quantity__InputValue-bQwA-dl OfyKm"]').text_content():
+        logger.info("Item quantity in the cart is increased")
+    else:
+        logger.info("Item quantity in the cart isn\'t increased")
 
     # Check out screenshot
     page.screenshot(path="Screenshots/add_to_cart/increasing quantity.png")
 
 
-def test_removing(login):
+def test_removing(login, logger):
     # Constructors
     page = login
-    add_to_cart = Add_To_Cart
 
     # Decreasing quantity
     page.frame_locator('.zoid-visible').get_by_alt_text('minus').click()
+
+    if "1" in page.frame_locator('.zoid-visible').locator(
+            '[class="change-quantity__InputValue-bQwA-dl OfyKm"]').text_content():
+        logger.info("Item quantity in the cart is decreased")
+    else:
+        logger.info("Item quantity in the cart isn\'t decreased")
 
     # Check out screenshot
     page.screenshot(path="Screenshots/add_to_cart/decreasing quantity.png")
 
 
-def test_deleting(login):
+def test_deleting(login, logger):
     # Constructors
     page = login
-    add_to_cart = Add_To_Cart
 
     # Deleting quantity
     page.frame_locator('.zoid-visible').locator('[class="cart-item-details-styled__DeleteIcon-dGLHPX dUlxRa"]').click()
+
+    if not page.frame_locator('.zoid-visible').locator(
+            '[class="change-quantity__InputValue-bQwA-dl OfyKm"]').is_visible():
+        logger.info("Items in the cart are deleted")
+    else:
+        logger.info("Items in the cart aren\'t deleted")
 
     # Check out screenshot
     page.screenshot(path="Screenshots/add_to_cart/deleting item.png")
